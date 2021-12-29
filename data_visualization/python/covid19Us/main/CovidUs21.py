aee8c8f8b35946adbb8ed302a2f9564f8655c928
unknown <1522192221@qq.com>
2021-12-29 20:15:28 +0800

    .

---
+++ data_visualization/python/covid19Us/main/CovidUs21.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+dir = "csse_covid_19_daily_reports_us"
+data_list = os.listdir(dir)
+data21 = [data for data in data_list if data[6:]=='2021.csv']
+data21_len = len(data21)
+print(data21)
+print("="*100)
+dframe = None
+for i in range(data21_len-1):
+    pre_frame = pd.read_csv(dir+'/'+data21[i])
+    next_frame = pd.read_csv(dir+'/'+data21[i+1])
+    confimed_sub = next_frame['Confirmed'] - pre_frame['Confirmed']
+    death_sub = next_frame['Deaths'] - pre_frame['Deaths']
+    recovered_sub = next_frame['Recovered'] - pre_frame['Recovered']
+    next_frame['Confirmed'] = confimed_sub
+    next_frame['Deaths'] = death_sub
+    next_frame['Recovered'] = death_sub
+    if dframe is None:
+        dframe = next_frame
+    else:
+        dframe = pd.concat([dframe,next_frame],ignore_index=True)
+print(dframe)
+dframe.to_csv("d.csv")
+
+
+
+
+
+
